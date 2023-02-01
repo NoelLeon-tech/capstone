3e74ccb52c1fb85b483eec84e75c8c5045b8a4a1
NoelLeon-tech <106872918+NoelLeon-tech@users.noreply.github.com>
2023-02-01 18:01:54 +0800

    deploy3

--- capstone/settings.py
+++ capstone/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-au(r55vw@0%3_=i#cf)m6
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
+CSRF_TRUSTED_ORIGINS = ["*"]
 
 # Application definition
 
